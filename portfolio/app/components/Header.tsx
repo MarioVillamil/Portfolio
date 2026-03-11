cb5bc34f37d64aca812c0ce87fa43b322c461213
MarioVillamil <MAVIL177@ulaval.ca>
2026-03-10 22:52:47 -0400

    navigation added

--- portfolio/app/components/Header.tsx
+++ portfolio/app/components/Header.tsx
@@ -1,8 +1,11 @@
+import Navigation from "./Navigation"
 export default function Header(){
     return (
         < header className=" w-full bg-olive-600 text-white">
            <div className=" max-w-6xl mx-auto items-center justify-between py-6 px-6">
              <h1 className="text-lg font-medium">Mario Villamil</h1>
+             <div className="ml-auto"></div>
+             <Navigation/>
            </div>
         </header>
     )
---
+++ portfolio/app/components/Navigation.tsx
@@ -0,0 +1,11 @@
+export default function Navigation (){
+    return(
+       <nav className="flex items-center gap-8 text-l text-gray-400">
+      <a href="#">Qui suis-je</a>
+      <a href="#">Stack</a>
+      <a href="#">Curriculum Vitae</a>
+      <a href="#">Projects</a>
+      <a href="#">Contact</a>
+    </nav>
+    )
+}
