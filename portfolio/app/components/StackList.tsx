d7cda7d75e6196cdb0264d76b5e33adf5ce3d02d
MarioVillamil <MAVIL177@ulaval.ca>
2026-03-17 21:13:40 -0400

    debut de issue 3

---
+++ portfolio/app/components/StackList.tsx
@@ -0,0 +1,28 @@
+export default function StackList(){
+    interface InavItem {
+        label : string,
+        type: "language" | "frontend" | "styling" | "ui"
+    }
+    const stackList: InavItem[] = [
+  { label: "C++", type: "language" },
+  { label: "Java", type: "language" },
+  { label: "Python", type: "language" },
+  { label: "TypeScript", type: "language" },
+
+  { label: "React", type: "frontend" },
+  { label: "Next.js", type: "frontend" },
+
+  { label: "Tailwind CSS", type: "styling" },
+
+  { label: "shadcn/ui", type: "ui" }
+]
+    return(
+        <div>
+            <ul>
+                {stackList.map((item, index) => (
+          <li key={index}>{item.label}</li>
+        ))}
+            </ul>
+        </div>
+    )
+}
